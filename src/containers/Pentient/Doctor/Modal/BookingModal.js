c4b7ff12423244ba0652949f40769be4d9a78eaa
yuana2002 <trinhhuuquoc36@gmail.com>
2023-07-11 21:08:41 +0700

    83

--- src/containers/Pentient/Doctor/Modal/BookingModal.js
+++ src/containers/Pentient/Doctor/Modal/BookingModal.js
@@ -3,6 +3,8 @@ import { connect } from "react-redux";
 import { FormattedMessage } from 'react-intl';
 import './BookingModal.scss';
 import { Modal } from 'reactstrap';
+import ProfileDoctor from '../ProfileDoctor';
+import _ from 'lodash';
 
 class BookingModal extends Component {
     constructor(props) {
@@ -23,6 +25,10 @@ class BookingModal extends Component {
 
     render() {
         let { isOpenModal, closeBookingClose, dataTime } = this.props;
+        let doctorId = '';
+        if (dataTime && !_.isEmpty(dataTime)) {
+            doctorId = dataTime.doctorId
+        }
         return (
             <Modal isOpen={isOpenModal}
                 className='booking-modal-container'
@@ -40,11 +46,13 @@ class BookingModal extends Component {
                     </div>
                     <div className='booking-modal-body'>
                         <div className='doctor-infor'>
-
+                            <ProfileDoctor
+                                doctorId={doctorId}
+                            />
                         </div>
-                        <div className='price'>
+                        {/* <div className='price'>
                             Giá khám 500.000VND
-                        </div>
+                        </div> */}
                         <div className='row'>
                             <div className='col-6 form-group'>
                                 <label>Họ tên</label>
--- src/services/userService.js
+++ src/services/userService.js
@@ -58,9 +58,13 @@ const getExtraInforDoctorById = (doctorId) => {
     return axios.get(`/api/get-extra-infor-doctor-by-id?doctorId=${doctorId}`)
 }
 
+const getProfileDoctorById = (doctorId) => {
+    return axios.get(`/api/get-profile-doctor-by-id?doctorId=${doctorId}`)
+}
+
 export {
     handleLoginApi, getAllUsers, createNewUserService,
     deleteUserService, editUserService, getAllCodeService, getTopDoctorHomeService, getAllDoctors,
     saveDetailDoctorService, getDetailInforDoctor, saveBulkScheduleDoctor, getScheduleDoctorByDate,
-    getExtraInforDoctorById
+    getExtraInforDoctorById, getProfileDoctorById
 }
