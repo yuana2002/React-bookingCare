f27730c6ba63125b8388f008499d58036f166751
yuana2002 <trinhhuuquoc36@gmail.com>
2023-07-13 11:08:51 +0700

    90

--- src/services/userService.js
+++ src/services/userService.js
@@ -70,9 +70,14 @@ const postVerifyBookingAppointment = (data) => {
     return axios.post('/api/verify-book-appointment', data)
 }
 
+const createNewSpecialty = (data) => {
+    return axios.post('/api/create-new-specialty', data)
+}
+
 export {
     handleLoginApi, getAllUsers, createNewUserService,
     deleteUserService, editUserService, getAllCodeService, getTopDoctorHomeService, getAllDoctors,
     saveDetailDoctorService, getDetailInforDoctor, saveBulkScheduleDoctor, getScheduleDoctorByDate,
-    getExtraInforDoctorById, getProfileDoctorById, postPatientBookingAppointment, postVerifyBookingAppointment
+    getExtraInforDoctorById, getProfileDoctorById, postPatientBookingAppointment, postVerifyBookingAppointment,
+    createNewSpecialty
 }
